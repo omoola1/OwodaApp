9c5c54c6921dcfccba849b24d2ae2e004583036b
support <support@MUMIN-OLABIYI>
2021-11-07 09:05:14 -0800

    Add project files.

---
+++ AgberoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AgberoApp
+{
+    class Program
+    {
+        int n;
+        static void Main(string[] args)
+        {
+            Ticket TicketSummary = new Ticket();
+            TicketSummary.Operation();
+        }
+    }
+    
+}
+
+      
+   
+
+
+
